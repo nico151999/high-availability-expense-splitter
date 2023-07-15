f2e55ea9e88250bd3b62a33a183bdb02a7a30e7f
Nico Feulner <nico.feulner@gmail.com>
2023-07-15 21:35:22 +0200

    feat: add some UI code

---
+++ frontend/expense_splitter/src/routes/(app)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    function goLevelUp() {
+        goto('./');
+    }
+</script>
+
+<button on:click={goLevelUp}>Navigate level up</button>
+<slot></slot>
--- frontend/expense_splitter/src/routes/+page.svelte
+++ frontend/expense_splitter/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    function gotoGroups() {
+        goto('./group');
+    }
+</script>
+
+<h2>My main page</h2>
+<button on:click={gotoGroups}>Open Groups</button>
+
+<slot></slot>
