7c9c8e9f696ea7f3b6e3a44361e0017b1fcf415e
Nico Feulner <nico.feulner@gmail.com>
2023-08-20 17:41:05 +0200

    feat: add category to frontend

---
+++ frontend/expense_splitter/src/routes/(app)/groups/[groupId=groupId]/(group)/categories/[categoryId=categoryId]/(category)/+page.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { createPromiseClient } from "@bufbuild/connect";
+	import { onDestroy, onMount } from "svelte";
+	import { writable } from "svelte/store";
+	import type { Category } from "../../../../../../../../../../../gen/lib/ts/common/category/v1/category_pb";
+	import { CategoryService } from "../../../../../../../../../../../gen/lib/ts/service/category/v1/service_connect";
+	import { streamCategory } from "../../utils";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+
+	const categoryClient = createPromiseClient(CategoryService, data.grpcWebTransport);
+	let category: Category | undefined;
+	const abortController = new AbortController();
+    let editMode = false;
+
+	const editedCategory = writable({
+		name: ''
+	});
+
+	onDestroy(() => {
+		abortController.abort();
+	});
+
+	onMount(async () => {
+		const res = await streamCategory(categoryClient, data.categoryId, abortController, (p) => {
+			category = p;
+		});
+        if (!res) {
+            console.log('Navigating level up due to no longer existing category');
+            goto('./');
+        }
+	});
+
+	async function updateCategory() {
+		try {
+			const res = await categoryClient.updateCategory({
+				categoryId: data.categoryId,
+				updateFields: [
+					{
+						updateOption: {
+							case: 'name',
+							value: $editedCategory.name
+						}
+					}
+				]
+			});
+			console.log('Updated category', res.category);
+            editMode = false;
+		} catch (e) {
+			console.error('An error occurred trying to update category', e);
+		}
+	}
+
+    function startEdit() {
+        if (!category) {
+            return;
+        }
+        editedCategory.set({
+            name: category.name
+        })
+        editMode = true;
+    }
+
+    function stopEdit() {
+        editMode = false;
+    }
+</script>
+
+<h2>Your category with ID {data.categoryId}</h2>
+<table>
+	<thead>
+		<th>Name</th>
+		<th>Action</th>
+	</thead>
+	<tbody>
+		{#if category}
+            {#if editMode}
+                <tr>
+                    <td><input type="text" placeholder="Category name" bind:value={$editedCategory.name}/></td>
+                    <td>
+                        <button on:click={updateCategory}>Update category</button>
+                        <button on:click={stopEdit}>Cancel</button>
+                    </td>
+                </tr>
+            {:else}
+                <tr>
+                    <td>{category.name}</td>
+                    <td><button on:click={startEdit}>Update category</button></td>
+                </tr>
+            {/if}
+		{:else}
+			<tr>Loading category...</tr>
+		{/if}
+	</tbody>
+</table>
---
+++ frontend/expense_splitter/src/routes/(app)/groups/[groupId=groupId]/(group)/categories/utils.ts
@@ -0,0 +1,88 @@
+import { Code, ConnectError, type PromiseClient } from "@bufbuild/connect";
+import { get, type Writable } from "svelte/store";
+import type { Category } from "../../../../../../../../../gen/lib/ts/common/category/v1/category_pb";
+import type { CategoryService } from "../../../../../../../../../gen/lib/ts/service/category/v1/service_connect";
+
+export async function streamCategory(
+	categoryClient: PromiseClient<typeof CategoryService>,
+	categoryID: string,
+	abortController: AbortController,
+    onCategoryUpdate: (category: Category) => void
+): Promise<boolean> {
+	try {
+		for await (const pRes of categoryClient.streamCategory({categoryId: categoryID}, {signal: abortController.signal})) {
+			if (pRes.update.case === 'stillAlive') {
+				continue;
+			}
+			onCategoryUpdate(pRes.update.value!);
+		}
+    } catch (e) {
+        if (e instanceof ConnectError) {
+            if (e.code === Code.Canceled) {
+                console.log(`Intentionally aborted category ${categoryID} stream`);
+                return true;
+            } else if (e.code === Code.DataLoss) {
+                console.log(`Category with ID ${categoryID} no longer exists`);
+                return false;
+            }
+        }
+        console.error('An error occurred trying to stream category.', e);
+    }
+    console.log(`Ended category ${categoryID} stream. Starting new one in 5 seconds.`);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    return await streamCategory(categoryClient, categoryID, abortController, onCategoryUpdate);
+}
+
+export async function streamCategories(
+    categoryClient: PromiseClient<typeof CategoryService>,
+    groupId: string,
+    abortController: AbortController,
+    categoriesStore: Writable<Map<string, {category?: Category, abortController: AbortController}> | undefined>
+) {
+    try {
+        for await (const cIDsRes of categoryClient.streamCategoryIdsInGroup({
+            groupId: groupId
+        }, {signal: abortController.signal})) {
+            if (cIDsRes.update.case === 'stillAlive') {
+                continue;
+            }
+            const categoryIDs = cIDsRes.update.value!.categoryIds;
+            let categories = get(categoriesStore);
+            if (categories === undefined) {
+                categories = new Map();
+            }
+            for (const cID of categories.keys()) {
+                if (categoryIDs.includes(cID)) {
+                    // remove element from items that are to be processed because it already exists
+                    categoryIDs.splice(categoryIDs.indexOf(cID), 1);
+                } else {
+                    // remove categories that are not present any more
+                    categories!.get(cID)!.abortController.abort();
+                    categories!.delete(cID);
+                }
+            }
+            for (const pID of categoryIDs) {
+                const abortController = new AbortController();
+                categories.set(pID, {
+                    abortController: abortController
+                });
+                streamCategory(categoryClient, pID, abortController, (category) => {
+                    categoriesStore.set(categories?.set(pID, {
+                        abortController: abortController,
+                        category: category
+                    }));
+                });
+            }
+            categoriesStore.set(categories);
+        }
+    } catch (e) {
+        if (e instanceof ConnectError && e.code === Code.Canceled) {
+            console.log('Intentionally aborted categories stream');
+            return;
+        }
+        console.error('An error occurred trying to stream categories', e);
+    }
+    console.log(`Ended categories stream. Starting new one in 5 seconds.`);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    await streamCategories(categoryClient, groupId, abortController, categoriesStore);
+}
