bfa1a0b9ecbe235a6414e6cf6087cdee0251e826
Nico Feulner <nico.feulner@gmail.com>
2023-09-25 01:13:23 +0200

    feat: costs per person

--- internal/service/expensestake/stream_expensestake_ids_in_group.go
+++ internal/service/expensestake/stream_expensestake_ids_in_group.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"connectrpc.com/connect"
+	expensev1 "github.com/nico151999/high-availability-expense-splitter/gen/lib/go/common/expense/v1"
 	expensestakev1 "github.com/nico151999/high-availability-expense-splitter/gen/lib/go/common/expensestake/v1"
 	expensestakesvcv1 "github.com/nico151999/high-availability-expense-splitter/gen/lib/go/service/expensestake/v1"
 	"github.com/nico151999/high-availability-expense-splitter/pkg/connect/errors"
@@ -18,17 +19,17 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-var streamExpenseStakeIdsInExpenseAlive = expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse{
-	Update: &expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse_StillAlive{},
+var streamExpenseStakeIdsInGroupAlive = expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse{
+	Update: &expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse_StillAlive{},
 }
 
-func (s *expensestakeServer) StreamExpenseStakeIdsInExpense(ctx context.Context, req *connect.Request[expensestakesvcv1.StreamExpenseStakeIdsInExpenseRequest], srv *connect.ServerStream[expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse]) error {
+func (s *expensestakeServer) StreamExpenseStakeIdsInGroup(ctx context.Context, req *connect.Request[expensestakesvcv1.StreamExpenseStakeIdsInGroupRequest], srv *connect.ServerStream[expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse]) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Hour)
 	defer cancel()
 
-	if err := service.StreamResource(ctx, s.natsClient.Conn, fmt.Sprintf("%s.*", environment.GetExpenseStakeSubject("*", req.Msg.GetExpenseId(), "*")), func(ctx context.Context) (*expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse, error) {
-		return sendCurrentExpenseStakeIdsInExpense(ctx, s.dbClient, req.Msg.GetExpenseId())
-	}, srv, &streamExpenseStakeIdsInExpenseAlive); err != nil {
+	if err := service.StreamResource(ctx, s.natsClient.Conn, fmt.Sprintf("%s.*", environment.GetExpenseStakeSubject(req.Msg.GetGroupId(), "*", "*")), func(ctx context.Context) (*expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse, error) {
+		return sendCurrentExpenseStakeIdsInGroup(ctx, s.dbClient, req.Msg.GetGroupId())
+	}, srv, &streamExpenseStakeIdsInGroupAlive); err != nil {
 		if eris.Is(err, errSelectExpenseStakeIds) {
 			return errors.NewErrorWithDetails(
 				ctx,
@@ -81,18 +82,29 @@ func (s *expensestakeServer) StreamExpenseStakeIdsInExpense(ctx context.Context,
 	return nil
 }
 
-func sendCurrentExpenseStakeIdsInExpense(ctx context.Context, dbClient bun.IDB, expenseId string) (*expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse, error) {
+func sendCurrentExpenseStakeIdsInGroup(ctx context.Context, dbClient bun.IDB, groupId string) (*expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse, error) {
 	log := logging.FromContext(ctx)
 
 	var expensestakeIds []string
-	if err := dbClient.NewSelect().Model((*expensestakev1.ExpenseStake)(nil)).Where("expense_id = ?", expenseId).Column("id").Order("for_id ASC").Scan(ctx, &expensestakeIds); err != nil {
+	if err := dbClient.NewSelect().
+		Model((*expensestakev1.ExpenseStake)(nil)).
+		Where(
+			"expense_id IN (?)",
+			dbClient.NewSelect().
+				Model((*expensev1.Expense)(nil)).
+				Column("id").
+				Where("group_id = ?", groupId),
+		).
+		Column("id").
+		Order("for_id ASC").
+		Scan(ctx, &expensestakeIds); err != nil {
 		log.Error("failed getting expense stake IDs", logging.Error(err))
 		// TODO: determine reason why expensestake IDs couldn't be fetched and return error-specific ErrVariable; e.g. use unit testing with dummy return values to determine potential return values unless there is something in the bun documentation
 		return nil, errSelectExpenseStakeIds
 	}
-	return &expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse{
-		Update: &expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse_Ids{
-			Ids: &expensestakesvcv1.StreamExpenseStakeIdsInExpenseResponse_ExpenseStakeIds{
+	return &expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse{
+		Update: &expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse_Ids{
+			Ids: &expensestakesvcv1.StreamExpenseStakeIdsInGroupResponse_ExpenseStakeIds{
 				Ids: expensestakeIds,
 			},
 		},
